4669fde3901d999420d35dc8b235bae7c30cad5f
John Leal <jlealgom@unal.edu.co>
2021-04-17 10:26:54 -0400

    Se crearon los archivos para una línea de vectores

---
+++ examples/vectors/vectors_test.cc
@@ -0,0 +1,26 @@
+// Definiciones de prueba. ¡No edite este archivo!
+
+#include <tuple>
+#include <vector>
+#define CATCH_CONFIG_MAIN // define main() automáticamente
+#include "catch.hpp"
+
+#include "student_vectors.hh" // implementaciones del estudiante
+
+// ===========================
+// pruebas para el ejercicio 1
+// ---------------------------
+
+TEST_CASE("Ex1", "[example]")
+{
+    REQUIRE(add(1, 1) == 2);
+}
+
+// ===========================
+// pruebas para el ejercicio 2
+// ---------------------------
+
+TEST_CASE("Ex2", "[example]")
+{
+    REQUIRE(mul(3, 3) == 9);
+}
